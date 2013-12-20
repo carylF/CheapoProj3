b459f06ff2940902f6f002319b5a4fc2da4595bb
Caryl <caryl.r.ford@gmail.com>
2013-12-19 22:36:15 -0500

    Update messages_b.js

--- messages_b.js
+++ messages_b.js
@@ -2,25 +2,35 @@ window.onload = function()
 {
 	//Removes No new Messages if there are messages present
 	message_retrieve();
-	document.getElementById('compose').onclick = compose;
-}
-
-function validate()
-{
-	//Regular Expressions -- REGEX for ensuring that pssword have at least
-	//one number one letter, and a captial letter and have to be at least 8 
-	//characters long 
+	setInterval(function(){message_retrieve()},120000);
+	document.getElementById('compose').onclick = compose
 }
 
 function compose()
 {
-	var array = new Array('to','from','subject');
+	var array = new Array('to','subject');
 
-	var send = document.createElement('button');
-	send.style.height='2.5em';
-	send.style.width = '5em';
-	//send.onclick = Call to the Ajax function dealing with the sending of messages
+	var send = document.createElement('p');
+	send.style.fontSize='1em';
+	send.innerText = 'Send';
+	send.onmouseover = function(){send.style.color = 'red';
+					   send.backgroundColor = 'blue';};
+	send.onmouseout = function(){send.style.color ='black';
+					 send.style.backgroundColor= '#EEEEEE';};
+	send.style.display = 'inline';
+	send.onclick = message_send;
 
+	var close = document.createElement('p');
+	close.style.fontSize ='1em';
+	close.innerText='Close';
+	close.style.display='inline';
+	close.style.paddingLeft = '40px';
+	close.onmouseover = function(){close.style.color = 'red';
+					    close.backgroundColor = 'blue';};
+	close.onmouseout = function(){close.style.color ='black';
+					 close.style.backgroundColor= '#EEEEEE';};
+	close.onclick = message_close;
+	
 	var compose_holder = document.createElement('div');
 	compose_holder.className = 'compose_holder';
 	compose_holder.style.zIndex = 10;
@@ -29,9 +39,13 @@ function compose()
 	var compose_body = document.createElement('textarea');
 	compose_body.rows  ='5';
 	compose_body.cols = '50';
+	compose_body.style.position = 'absolute';
+	compose_body.style.left = '0px'
+
 
 	var compose_form = document.createElement('form')
 	compose_form.action = '#';
+	compose_form.method = 'get';
 
 	doc_divs = document.getElementsByTagName('div');
 	doc_divs[4].insertAdjacentElement('afterBegin',compose_form);
@@ -39,66 +53,118 @@ function compose()
 	for(var i=0;i<array.length;i++)
 	{
 		var compose_label = document.createElement('label');
+		compose_label.className = 'popuplabel';
+		compose_label.style.fontSize = '14px';
 		compose_label.innerText =array[i].toUpperCase();
 		var compose_input = document.createElement('input');
 		compose_input.type='text';
 		compose_input.name = array[i];
+		compose_input.className = 'messageDetails';
 		compose_input.style.position = 'absolute';
-		compose_input.style.left          = ''
+		compose_input.style.left          = '250px';
 
 		compose_form.insertAdjacentElement('beforeEnd',compose_label);
 		compose_form.insertAdjacentElement('beforeEnd',compose_input);
 		compose_form.insertAdjacentHTML('beforeEnd','<br>');
 	}
 
+
 	compose_form.insertAdjacentElement('beforeEnd',compose_body);
-
+	compose_form.insertAdjacentHTML('beforeEnd','<br><br>');
 	compose_form.insertAdjacentElement('beforeEnd', send);
-
-
-	// document.getElementById('content').insertAdjacentElement('beforeEnd',compose_holder);
-	// //compose_holder.innerHTML = document.createElement('form');
-	// doc_divs = document.getElementsByTagName('div');
-	// compose_form = document.createElement('form');
-	// doc_divs[4].insertAdjacentElement('afterBegin',compose_form);
-
-	// compose_holder.innerHTML.innerHTML = document.createElement('input');
-	// compose_holder.innerHTML.innerHTML.type = 'text';
-	// compose_holder.innerHTML.innerHTML.name = 'to';
-	// compose_holder.innerHTML.innerHTML = document.createElement('input');
-	// compose_holder.innerHTML.innerHTML.type = 'text';
-	// compose_holder.innerHTML.innerHTML.name = 'from';
-	// compose_holder.innerHTML.innerHTML = document.createElement('input');
-	// compose_holder.innerHTML.innerHTML.type = 'text';
-	// compose_holder.innerHTML.innerHTML.name='subject';
-	// compose_holder.innerHTML.innerHTML = document.createElement('input');
-	// compose_holder.innerHTML.innerHTML.type = 'text';
-	// compose_holder.innerHTML.innerHTML.name='content';
-
+	compose_form.insertAdjacentElement('beforeEnd',close);
 }
 
-function printData(data)
+function parseData(data)
+{//Parses the XML data that was sent in to retrieve the message for this user 
+	var detail_titles = new Array('ID','TO','FROM','SUBJECT','BODY');
+	var response_data = data.responseXML
+	if(response_data != null)
+	{
+		$('content').removeChild($('content').childNodes[0]);
+	}
+	var messagecount =response_data.getElementsByTagName('MESSAGE');
+	message_holder = document.createElement('div');
+	message_holder.className = 'messageHolder';
+	message_holder.style.backgroundColor = '#3B8C8B';
+	$('content').insertAdjacentElement('beforeEnd',message_holder);
+	for(var i=0;i<messagecount.length;i++)
+	{
+		messagecontent= messagecount[i].children;
+		var messagediv = document.createElement('div');
+		messagediv.className = 'message';
+		message_holder.insertAdjacentElement('afterBegin',messagediv);
+		for(var j=0;j<messagecontent.length;j++)
+		{
+			
+			var messageDetails =  detail_titles[j]+':';
+			if(detail_titles[j] == 'BODY')
+			{
+				messageDetails +=  '<br>'+messagecontent[j].textContent;	
+			}
+			else if (detail_titles[j] == 'SUBJECT' || detail_titles[j] == 'ID' || detail_titles[j] == 'FROM')
+			{
+				messageDetails += '&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;'+messagecontent[j].textContent;
+			}
+			messagediv.insertAdjacentHTML('beforeEnd',messageDetails);
+			messagediv.insertAdjacentHTML('beforeEnd','<br>');
+		}
+	}
+	//$('content').replaceChild($('content').innerText,|Replace with data|);
+	
+}
+
+function message_read()
 {
-	alert(data.responseXML);
+	
 }
 
 function message_retrieve()
 {
+	
 	new Ajax.Request("action.php",
 	{
 		parameters: {a:'getmessage'},
 		method: "get",
-		onSuccess: printData,
+		onSuccess: parseData,
 		onCreate: function(response){
 			var t = response.transport;
 			t.setRequestheader = t.setRequestHeader.wrap(function(original, k, v){
 				if(/^(accept|accept-language|content-lanuage)$/i.test(k))
 					return original(k,v);
 				if(/^content-type$/i.test(k) &&
-					/^ (application\/x-form-urlencoded|multipart\/form-data|text\/plain)(;.+?$/i.text(v))
+					/^ (application\/x-form-urlencoded|multipart\/form-data|text\/plain)(;.+)?$/i.text(v))
 					return original(k,v);
 				return;});
 			}
 	});
 }
-	
+
+function message_send()
+{
+	var mesdet = document.getElementsByClassName('messageDetails');
+	var message = document.getElementsByTagName('textarea');
+	new Ajax.Request("action.php",
+	{
+		parameters: {a:'messagecompose', to:mesdet[0].value, subject:mesdet[1].value, body:message[0].value},
+		method: "get",
+		onSuccess: function(data){alert(data.responseText);},
+		onCreate: function(response){
+			var t = response.transport;
+			t.setRequestheader = t.setRequestHeader.wrap(function(original, k, v){
+				if(/^(accept|accept-language|content-lanuage)$/i.test(k))
+					return original(k,v);
+				if(/^content-type$/i.test(k) &&
+					/^ (application\/x-form-urlencoded|multipart\/form-data|text\/plain)(;.+)?$/i.text(v))
+					return original(k,v);
+				return;});
+			}
+	});
+}
+
+function message_close()
+{
+	var elemRemove = $$('div.compose_holder');
+	document.getElementById('content').removeChild(elemRemove[0]);
+}
+
